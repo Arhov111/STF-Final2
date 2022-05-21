15661ec5ba9b672eb5beae87e73d0e0a13997e79
Arhov111 <86516084+Arhov111@users.noreply.github.com>
2022-05-21 14:38:59 +0400

    Add files via upload

---
+++ src/main/java/locators/RegisterNowPageConstants.java
@@ -0,0 +1,11 @@
+package locators;
+
+public class RegisterNowPageConstants {
+    public static final String emailSection = "//*[@id=\"ctl00_Content_RgLogTextBox\"]";
+    public static final String passwordSection = "//*[@id=\"ctl00_Content_RgLogTextBox\"]";
+    public static final String repeatPasswordSection = "//*[@id=\"ctl00_Content_RepeatPasswordTextBox\"]";
+    public static final String invalidEmail = "//*[@id=\"ctl00_Content_StatusLabel\"]";
+    public static final String invalidPassword = "//*[@id=\"ctl00_Content_StatusLabel\"]";
+    public static final String agreeWithTC = "//*[@id=\"ctl00_Content_CheckBox1\"]";
+
+}
