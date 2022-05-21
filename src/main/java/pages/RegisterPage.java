3c35eb9954c852f5e7eca4732c4385a1ea504b65
Arhov111 <86516084+Arhov111@users.noreply.github.com>
2022-05-21 14:40:51 +0400

    Update RegisterPage.java

--- src/main/java/pages/RegisterPage.java
+++ src/main/java/pages/RegisterPage.java
@@ -10,6 +10,7 @@ public class RegisterPage extends BasePage{
     private By registerNowButton = By.xpath(RegisterPageConstants.registerNowButton);
 
 
+
     @Override
     public String getUrl() {
         return baseURL + "/am/log-in";
@@ -21,4 +22,5 @@ public class RegisterPage extends BasePage{
     public void clickRegisterNow() {
         click(registerNowButton);
     }
+
 }
